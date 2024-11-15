d8099de8a27744962c0e67fd8b93645cb7ecb081
Nailsonnela <nailsonnela048@gmail.com>
2024-11-15 17:00:45 +0000

    adiconar arquivo

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function home() {
+  return (
+    <h1>
+      renato,esse e o unico meio pra provar que eu quero continuar com
+      programçao,eu consegui subir todos os arquivos para o repositorio mais o
+      notebook nao esta suportando processar o codespaces como estava la no
+      começo do curso,eu estou em desevolvimento de sofware e agora a demanda de
+      pastas e arquivos mais que dobrou e ele nao esta processando como
+      deveria,por isso vou da um tempo na parte pratica e vou daqui por diante
+      so assistir as aulas,mais logo logo estarei entrando em contato com voce
+      pra esta comprando um notebook!!!!!
+    </h1>
+  );
+}
+export default home;
